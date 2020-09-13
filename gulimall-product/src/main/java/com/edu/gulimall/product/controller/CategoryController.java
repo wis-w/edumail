5a557a0cc55eff802179bad5b256c0a895a9d033
wis-w <1044957202@qq.com>
2020-09-13 20:44:42 +0800

    三级菜单拖动批量修改

--- gulimall-product/src/main/java/com/edu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/edu/gulimall/product/controller/CategoryController.java
@@ -48,7 +48,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -61,6 +61,18 @@ public class CategoryController {
 
         return R.ok();
     }
+
+    /**
+     * 批量修改
+     */
+    @RequestMapping("/update/sort")
+    // @RequiresPermissions("product:category:update")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
+
+        return R.ok();
+    }
+
 
     /**
      * 修改
